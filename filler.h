5b743f3327235337ebe8575db7881c11e66356d6
MixFon <mixfon@rambler.ru>
2019-03-29 20:32:21 +0300

    filler

--- filler.h
+++ filler.h
@@ -10,7 +10,15 @@ typedef struct	s_plyer
 	char		sym;
 	int			width;
 	int			heith;
+	char		**bourd;
 	
 }				t_plyer;
+
+typedef struct	s_token
+{
+	int			width;
+	int			heith;
+	char		**token;
+}				t_token;
 
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 ** Parsing width and heith bourd .
 */
 
-void	parsing_wh(char *line, t_plyer *pl)
+void	parsing_wh(char *line, int *width, int *heith)
 {
 	char *fir_num;
 	char *sec_num;
@@ -21,9 +21,45 @@ void	parsing_wh(char *line, t_plyer *pl)
 			sec_num = (line + 1);
 		line++;
 	}
-	pl->heith = ft_atoi(fir_num);
-	pl->width = ft_atoi(sec_num);
-	printf("heith = %d width = %d\n", pl->heith, pl->width);
+	*heith = ft_atoi(fir_num);
+	*width = ft_atoi(sec_num);
+}
+
+/*
+** Delete arr.
+*/
+
+void	dell_arr(char **arr)
+{
+	while(*arr != NULL)
+	{
+		ft_strdel(arr);
+		arr++;
+	}
+}
+
+/*
+** Infill array (bourd and token).
+*/
+
+char	**infill_arr(int hieth, int iter)
+{
+	int		i;
+	char	*line;
+	char	**arr;
+
+	arr = (char **)malloc(sizeof(char*) * (hieth + 1));
+	i = -1;
+	while (++i < hieth)
+	{
+		get_next_line(0, &line);
+		arr[i] = ft_strdup(line + iter);
+		write(2, arr[i], ft_strlen(arr[i]));
+		write(2, "\n", 1);
+		free(line);
+	}
+	arr[i] = NULL;
+	return (arr);
 }
 
 /*
@@ -48,44 +84,62 @@ t_plyer	*info_plyer()
 		pl->sym = '?';
 	free(line);
 	get_next_line(0, &line);
-	parsing_wh(line, pl);
+	parsing_wh(line,  &pl->width, &pl->heith);
 	printf("P %s\n", line);
 	printf("sym %c\n", pl->sym);
 	free(line);
+	printf("heith = %d width = %d\n", pl->heith, pl->width);
+	get_next_line(0, &line);
+	free(line);
+	pl->bourd = infill_arr(pl->heith, 4);
 	return (pl);
+}
+
+/*
+** Create token size widht_tok and heith_tok.
+*/
+
+t_token *create_token()
+{
+	t_token *tk;
+	char	*line;
+
+	tk = (t_token *)malloc(sizeof(t_token));
+	get_next_line(0, &line);
+	parsing_wh(line,  &tk->width, &tk->heith);
+	printf("tk->heith = %d tk->width = %d\n", tk->heith, tk->width);
+	free(line);
+	tk->token = infill_arr(tk->heith, 0);
+	return (tk);
+
 }
 
 int		main(int ac, char **av)
 {
 	t_plyer *pl;
-	char *line;
-	int	i;
+	t_token	*tk;
+	char	*line;
+	int		i;
 
 	
 	//get_next_line(0, &line);
 	pl = info_plyer();
-	//while (1)
-	//{
-		i = -1;
-		while(i < 66)
-		{
-			get_next_line(0, &line);
-			write(2, line, ft_strlen(line));
-			write(2, "\n", 1);
-			free(line);
-			i++;
-			//ft_printf("%d %d\n", i , i);
-		}
-		ft_putstr("3");
-		ft_putstr(" ");
-		ft_putstr("2");
-		ft_putstr("\n");
-		write(2, "a\n", 2);
+	//create_boudr(pl);
+	tk = create_token();
+	dell_arr(pl->bourd);
+	dell_arr(tk->token);
+	free(tk->token);
+	free(pl->bourd);
+	ft_putstr("3");
+	ft_putstr(" ");
+	ft_putstr("2");
+	ft_putstr("\n");
+	write(2, "a\n", 2);
 	//}
 	//exit(1);
 	//ft_printf("19 32\n");
 	//write(2, "Hello\n", 6);
-	free(line);
+	//free(line);
 	free(pl);
 	return (0);
 }
