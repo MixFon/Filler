e6a57a04e5a18bd530c9d433cb0c02efb38ffa5d
MixFon <mixfon@rambler.ru>
2019-03-31 09:26:47 +0300

    filler

--- filler.h
+++ filler.h
@@ -5,7 +5,7 @@
 #include "libft/libft.h"
 #include "libft/get_next_line.h"
 
-typedef struct	s_plyer
+typedef struct	s_bourd
 {
 	char		sym;
 	int			width;
@@ -14,13 +14,16 @@ typedef struct	s_plyer
 	int			in_x;
 	int			in_y;
 	
-}				t_plyer;
+}				t_bourd;
 
 typedef struct	s_token
 {
 	int			width;
 	int			heith;
 	char		**token;
+	int			coor_x[20];
+	int			coor_y[20];
+	int			iter;
 }				t_token;
 
 #endif
--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include "filler.h"
 
 /*
-** Parsing width and heith bourd .
+** Parsing width and heith bourd and token.
 */
 
 void	parsing_wh(char *line, int *width, int *heith)
@@ -63,36 +63,77 @@ char	**infill_arr(int hieth, int iter)
 }
 
 /*
-** Collect information abour plyer and the bourd.
+** Collect information abour the bourd.
 */
 
-t_plyer	*info_plyer()
+t_bourd	*info_bourd()
 {
 	int		i;
 	char	*line;
-	t_plyer *pl;
+	t_bourd *br;
 
-	if(!(pl = (t_plyer *)malloc(sizeof(t_plyer))))
+	if(!(br = (t_bourd *)malloc(sizeof(t_bourd))))
 		return (NULL);
 	get_next_line(0, &line);
 	printf("line %s\n", line);
 	if (line[10] == '1')
-		pl->sym = 'O';
+		br->sym = 'O';
 	else	if (line[10] == '2')
-		pl->sym = 'X';
+		br->sym = 'X';
 	else 
-		pl->sym = '?';
+		br->sym = '?';
 	free(line);
 	get_next_line(0, &line);
-	parsing_wh(line,  &pl->width, &pl->heith);
+	parsing_wh(line,  &br->width, &br->heith);
 	printf("P %s\n", line);
-	printf("sym %c\n", pl->sym);
+	printf("sym %c\n", br->sym);
 	free(line);
-	printf("heith = %d width = %d\n", pl->heith, pl->width);
+	printf("heith = %d width = %d\n", br->heith, br->width);
 	get_next_line(0, &line);
 	free(line);
-	pl->bourd = infill_arr(pl->heith, 4);
-	return (pl);
+	br->bourd = infill_arr(br->heith, 4);
+	return (br);
+}
+
+/*
+** Print coordinates token.
+*/
+
+void	print_coor_tok(t_token *tk)
+{
+	int	i;
+
+	i = -1;
+	while (++i < tk->iter)
+		printf("i = %d, coor_x =  %d, coor_y %d\n", i, tk->coor_x[i], tk->coor_y[i]);
+}
+
+/*
+** Infill coordinates x and y for token.
+*/
+
+void	infill_coor_tok(t_token *tk)
+{
+	int	i;
+	int	j;
+	int	iter;
+
+	i = -1;
+	tk->iter = 0;
+	while (++i < tk->heith)
+	{
+		j = -1;
+		while (++j < tk->width)
+		{
+			if (tk->token[i][j] == '*')
+			{
+				tk->coor_x[tk->iter] = i;
+				tk->coor_y[tk->iter] = j;
+				tk->iter++;
+			}
+		}
+	}
+	print_coor_tok(tk);
 }
 
 /*
@@ -110,6 +151,7 @@ t_token *create_token()
 	printf("tk->heith = %d tk->width = %d\n", tk->heith, tk->width);
 	free(line);
 	tk->token = infill_arr(tk->heith, 0);
+	infill_coor_tok(tk);
 	return (tk);
 }
 
@@ -117,43 +159,40 @@ t_token *create_token()
 ** Reading bourd.
 */
 
-void	read_bourd(t_plyer *pl)
+void	read_bourd(t_bourd *br)
 {
 	int	i;
 	int	j;
 
-	i = 0;
-	while(i < pl->heith)
+	i = -1;
+	while(++i < br->heith)
 	{
-		j = 0;
-		while(j < pl->width)
+		j = -1;
+		while(++j < br->width)
 		{
-			if (pl->bourd[i][j] == pl->sym)
+			if (br->bourd[i][j] == br->sym)
 			{
-				pl->in_x = i;
-				pl->in_y = j;
+				br->in_x = i;
+				br->in_y = j;
 			}
-			j++;
 		}
-		i++;
 	}
 }
 
 int		main(int ac, char **av)
 {
-	t_plyer *pl;
+	t_bourd *br;
 	t_token	*tk;
 	char	*line;
-	int		i;
 
-	pl = info_plyer();
+	br = info_bourd();
 	tk = create_token();
-	read_bourd(pl);
-	printf("in_x = %d, in_y = %d", pl->in_x, pl->in_y);
-	dell_arr(pl->bourd);
+	read_bourd(br);
+	printf("in_x = %d, in_y = %d\n", br->in_x, br->in_y);
+	dell_arr(br->bourd);
 	dell_arr(tk->token);
 	free(tk->token);
-	free(pl->bourd);
-	free(pl);
+	free(br->bourd);
+	free(br);
 	return (0);
 }
